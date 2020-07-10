6c62b47e107f75cf34608ead18f0940553945818
Jeff Albert <jalbert@salesforce.com>
2020-07-10 12:11:36 -0400

    Adding back in integration testing as tests and integrationTests

---
+++ integrationTests/redisCachingDriver.test.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+"use strict";
+
+import { BaseClient, CacheManagerRedis } from "../src";
+import cacheTests from "../test/cacheHelpers/basic.test.helper";
+import etagTests from "../test/cacheHelpers/etag.test.helper";
+import evictionTests from "../test/cacheHelpers/eviction.test.helper";
+import multipleHeadersTests from "../test/cacheHelpers/multipleHeaders.test.helper";
+import noCacheHeaderTests from "../test/cacheHelpers/noCacheHeader.test.helper";
+import delayedTests from "../test/cacheHelpers/delayedTests.test.helper";
+
+describe("Redis cache tests", () => {
+  before(function () {
+    this.client = new BaseClient({
+      baseUri: "https://somewhere",
+      cacheManager: new CacheManagerRedis({
+        connection: "redis://localhost",
+        keyvOptions: { keepAlive: false },
+      }),
+    });
+  });
+  after(function () {
+    this.client.clientConfig.cacheManager.quit();
+  });
+  cacheTests();
+  etagTests();
+  evictionTests();
+  multipleHeadersTests();
+  noCacheHeaderTests();
+  delayedTests();
+});
