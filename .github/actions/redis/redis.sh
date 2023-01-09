5e78242e89f8511d1e504b348de3909a1765b0ab
Joel Uong <joel.uong@salesforce.com>
2023-01-09 17:42:07 -0500

    modiy action and redis.sh

--- .github/actions/redis/redis.sh
+++ .github/actions/redis/redis.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-docker run --name redis --publish 6379:6379 --detach redis:6
+REDIS_VERSION=$1
+REDIS_PORT=$2
+REDIS_CONTAINER_NAME=$3
+
+if [ -z "$REDIS_VERSION" ]; then
+  echo "Missing Redis version in the [redis-version] input. Received value: $REDIS_VERSION"
+  echo "Falling back to Redis version [latest]"
+  REDIS_VERSION='latest'
+fi
+
+echo "Starting single-node Redis instance"
+docker run --name $REDIS_CONTAINER_NAME --publish $REDIS_PORT:6379 --detach redis:$REDIS_VERSION
