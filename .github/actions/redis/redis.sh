be487b42ff5c3b319f6a4958125d977a552d820b
Joel Uong <joel.uong@salesforce.com>
2023-01-09 17:56:00 -0500

    revert redis.sh

--- .github/actions/redis/redis.sh
+++ .github/actions/redis/redis.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker run --name redis-server --publish 6379:6379 --detach redis:$INPUT_REDIS_VERSION 
+REDIS_VERSION=$1
+
+docker run --name redis-server --publish 6379:6379 --detach redis:$REDIS_VERSION
