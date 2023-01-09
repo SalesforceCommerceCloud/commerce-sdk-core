e3d33b4e2e7b423a59b1acf95d8b4e0b5c39b8c1
Joel Uong <joel.uong@salesforce.com>
2023-01-09 17:44:56 -0500

    remove args

--- .github/actions/redis/redis.sh
+++ .github/actions/redis/redis.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
 REDIS_VERSION=$1
-REDIS_PORT=$2
-REDIS_CONTAINER_NAME=$3
 
-if [ -z "$REDIS_VERSION" ]; then
-  echo "Missing Redis version in the [redis-version] input. Received value: $REDIS_VERSION"
-  echo "Falling back to Redis version [latest]"
-  REDIS_VERSION='latest'
-fi
-
-echo "Starting single-node Redis instance"
-docker run --name $REDIS_CONTAINER_NAME --publish $REDIS_PORT:6379 --detach redis:$REDIS_VERSION
+docker run --name redis-server --publish 6379:6379 --detach redis:$REDIS_VERSION
