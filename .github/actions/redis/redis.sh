c5fda76bd2cd57fc825aea988e33344300eca784
Joel Uong <joel.uong@salesforce.com>
2023-01-09 17:51:32 -0500

    modify redis.sh

--- .github/actions/redis/redis.sh
+++ .github/actions/redis/redis.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-REDIS_VERSION=$1
-
-docker run --name redis-server --publish 6379:6379 --detach redis:$REDIS_VERSION
+docker run --name redis-server --publish 6379:6379 --detach redis:$INPUT_REDIS_VERSION 
